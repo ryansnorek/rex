0bfe9ad36d9c794253fdcb8e8d7c14829acaa45f
Ryan <snorekr@gmail.com>
2021-11-27 09:51:14 -0500

    alignment posters

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,12 @@
 export default function Item({ item, category }) {
     return (
         <div className="item">
-            <img src={`https://image.tmdb.org/t/p/w500/${item.poster_path}`} alt="item" />
+            {item.poster_path ? <img src={`https://image.tmdb.org/t/p/w500/${item.poster_path}`} alt="poster"/> : <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2kjzLg9BzACM4Dibtz-bHQrtXKzdyEWhvzw&usqp=CAU" alt="poster" />}
             <div className="text">
                 {category === "tv" ? <h2>{item.original_name}</h2> : <h2>{item.title}</h2>}
-                <p>{item.overview}</p>
+                {item.overview ? <p className="overview">{item.overview}</p> : <p>Description unavailable</p>}
                 {category ==="tv" ? <p>First aired: {item.first_air_date}</p> : <p>Release date: {item.release_date}</p>}
+                <button>Rexy</button>
             </div>
         </div>
     )
