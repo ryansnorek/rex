f6e014433349492a4152f206df26b62d39f8ac14
Ryan <snorekr@gmail.com>
2021-11-28 10:34:26 -0500

    search icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ export default function Header() {
                 <div className="links">
                     <ul>
                         <li>
-                            <Link to="/search">{<img className="icon" src="../../images/search.png" alt="search"/>}</Link>
+                            <Link to="/search">{<img className="icon" src="../../images/magnifier.png" alt="search"/>}</Link>
                         </li>
                         <li>
                             <Link to="/account">{<img className="icon" src="../../images/user.png" alt="user-account"/>}</Link>
