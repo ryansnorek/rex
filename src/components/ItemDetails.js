91736a257d3278b52ab6b7db5db57d1fa468d48d
Ryan <snorekr@gmail.com>
2021-12-04 11:55:18 -0500

    item style

--- src/components/ItemDetails.js
+++ src/components/ItemDetails.js
@@ -7,32 +7,32 @@ import { addRexy } from "../actions";
 
 function ItemDetails({ dispatch, item }) {
     const { id } = useParams();
+    const movie = item.movie ? item.movie : [];
     
     const handleAdd = id => dispatch(addRexy(id));
 
     if (!item.movie) return <h1>If you do not see it, please refresh the page</h1>
     return (
         <div className="item-details">
-            <img className="backdrop" src={`${POSTER_URL}${item.movie.backdrop_path}`} alt=""/>
-            <h2>{item.movie.title}</h2>
-            <h3>{item.movie.tagline}</h3>
+            <img className="backdrop" src={`${POSTER_URL}${movie.backdrop_path}`} alt=""/>
+            <h2>{movie.title}</h2>
+            <h3>{movie.tagline}</h3>
+            <div className="genres">
+                {movie.genres.map(g => <p>{g.name}</p>)}
+            </div>
             <div className="text">
                 <div className="left">
-                    <div className="genres">
-                        <p>Genre: </p>{item.movie.genres.map(g => <p>{g.name}</p>)}
-                        <p>Budget: ${item.movie.budget.toLocaleString()}</p>
-                        <p>Revenue: ${item.movie.revenue.toLocaleString()}</p> 
-                        <p>Popularity: {item.movie.popularity}</p>
-                        <p>Released: {item.movie.release_date}</p>
-                    </div>
+                    <p>Budget: ${movie.budget.toLocaleString()}</p>
+                    <p>Revenue: ${movie.revenue.toLocaleString()}</p> 
+                    <p>Popularity: {movie.popularity}</p>
+                    <p>Released: {movie.release_date}</p>
                 </div>
                 <div className="right">
-                    <p>{item.movie.overview}</p>
+                    <p>{movie.overview}</p>
                 </div>
             </div>
             <div className="button-container">
-                <button>Details</button>
-                <button onClick={() => handleAdd(item.id)}>Add</button>
+                <button onClick={() => handleAdd(movie.id)}>Add to collection</button>
             </div>
         </div>
     )
