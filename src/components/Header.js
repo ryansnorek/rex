c9859a44137325afec33fa215f66b6ddbc5ca6c2
Ryan <snorekr@gmail.com>
2021-12-05 15:59:56 -0500

    navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,10 +13,10 @@ export default function Header() {
                             <Link to="/search">{<img className="icon" src="../../images/magnifier.png" alt="search"/>}</Link>
                         </li>
                         <li>
-                            <Link to="/account">{<img className="icon" src="../../images/user.png" alt="user-account"/>}</Link>
+                            <Link to="/friends">{<img className="icon" src="../../images/friends.png" alt="friends"/>}</Link>
                         </li>
                         <li>
-                            <Link to="/friends">{<img className="icon" src="../../images/friends.png" alt="friends"/>}</Link>
+                            <Link to="/account">{<img className="icon" src="../../images/user.png" alt="user-account"/>}</Link>
                         </li>
                     </ul>
             </nav>
