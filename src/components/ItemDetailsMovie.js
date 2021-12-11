f1508ebad1603a69ccd12be2c1f72a112ecd106c
Ryan <snorekr@gmail.com>
2021-12-11 12:37:39 -0500

    purp heart

--- src/components/ItemDetailsMovie.js
+++ src/components/ItemDetailsMovie.js
@@ -1,30 +1,39 @@
 import { POSTER_URL } from "../constants";
 
 function ItemDetailsMovie({ movie, handleAdd }) {
+    const { 
+        backdrop_path, title, tagline, genres, 
+        budget, revenue, vote_average, release_date, 
+        overview, id 
+        } = movie;
+         
     return (
         <div className="item-details">
-            <img className="backdrop" src={`${POSTER_URL}${movie.backdrop_path}`} alt=""/>
-            <h2>{movie.title}</h2>
-            <h3>{movie.tagline}</h3>
+            <img className="backdrop" src={`${POSTER_URL}${backdrop_path}`} alt=""/>
+            <h2>{title}</h2>
+            <h3>{tagline}</h3>
             <div className="genres">
-                {movie.genres.map(g => <p>{g.name}</p>)}
+                {genres && genres.map(g => <p>{g.name}</p>)}
             </div>
             <div className="text">
                 <div className="left">
-                    <p>Budget: ${movie.budget.toLocaleString()}</p>
-                    <p>Revenue: ${movie.revenue.toLocaleString()}</p> 
-                    <p>Popularity: {movie.popularity}</p>
-                    <p>Released: {movie.release_date}</p>
+                    <p className="heart">{vote_average}</p>
+                    {!vote_average && <p>no ratings yet</p>}
+                </div>
+                <div className="center">
+                    <p>Released: {release_date}</p>
+                    <p>Budget: ${budget === 0 ? " unavailable" : budget.toLocaleString()}</p>
+                    <p>Revenue: ${revenue === 0 ? " unavailable" : revenue.toLocaleString()}</p> 
                 </div>
                 <div className="right">
-                    <p>{movie.overview}</p>
+                    <p>{overview ? overview : "overview unavailable"}</p>
                 </div>
             </div>
             <div className="button-container">
-                <button onClick={() => handleAdd(movie.id)}>Add to collection</button>
+                <button onClick={() => handleAdd(id)}>Add to collection</button>
                 <a 
                     className="google-it"
-                    href={`https://www.google.com/search?q=what+is+${movie.title}+streaming+on&oq=what+is+dexter+streaming+on&aqs=chrome..69i57j0i7i30l2j0i8i30l7.27237j0j7&sourceid=chrome&ie=UTF-8`}
+                    href={`https://www.google.com/search?q=what+is+${title}+streaming+on&oq=what+is+dexter+streaming+on&aqs=chrome..69i57j0i7i30l2j0i8i30l7.27237j0j7&sourceid=chrome&ie=UTF-8`}
                     target="_blank" 
                     rel="noreferrer"
                 >Find which streaming service</a>
--- src/components/ItemDetailsTvShow.js
+++ src/components/ItemDetailsTvShow.js
@@ -1,28 +1,33 @@
 import { POSTER_URL } from "../constants";
 
 function ItemDetailsTvShow({ tvShow, handleAdd }) {
+    const { 
+        backdrop_path, name, tagline, genres, 
+        budget, revenue, vote_average, first_air_date, 
+        overview, id, networks 
+        } = tvShow;
     return (
         <div className="item-details">
-            <img className="backdrop" src={`${POSTER_URL}${tvShow.backdrop_path}`} alt=""/>
-            <h2>{tvShow.name}</h2>
-            {tvShow.tagline && <h3>{tvShow.tagline}</h3>}
+            <img className="backdrop" src={`${POSTER_URL}${backdrop_path}`} alt=""/>
+            <h2>{name}</h2>
+            {tagline && <h3>{tagline}</h3>}
+            <div className="genres">
+                {genres && genres.map(g => <p>{g.name}</p>)}
+            </div>
             <div className="text">
                 <div className="left">
-                    <p>Popularity: {tvShow.popularity}</p>
-                    <p>First Aired: {tvShow.first_air_date}</p>
+                    <p className="heart">{vote_average}</p>
+                </div>
+                <div className="center">
+                    <p>First Aired: {first_air_date}</p>
                 </div>
                 <div className="right">
-                    <p>{tvShow.overview}</p>
+                    <p>{overview}</p>
                 </div>
             </div>
             <div className="button-container">
-                <button onClick={() => handleAdd(tvShow.id)}>Add to collection</button>
-                <a 
-                    className="google-it"
-                    href={`https://www.google.com/search?q=what+is+${tvShow.name}+streaming+on&oq=what+is+dexter+streaming+on&aqs=chrome..69i57j0i7i30l2j0i8i30l7.27237j0j7&sourceid=chrome&ie=UTF-8`}
-                    target="_blank" 
-                    rel="noreferrer"
-                >Find which streaming service</a>
+                <button onClick={() => handleAdd(id)}>Add to collection</button>
+                {networks && networks.map(n => <p>{n.name}</p>)}
             </div>
         </div>
     )
