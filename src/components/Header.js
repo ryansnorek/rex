e9bc25d44b4e0e38ed23d34b911e6d0e6b92d408
Ryan <snorekr@gmail.com>
2021-12-25 08:33:36 -0500

    home icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,40 +2,62 @@ import { Link } from "react-router-dom";
 import { useNavigate } from "react-router";
 
 export default function Header() {
-    const navigate = useNavigate();
-    return (
-        <header>
-            <h1 className="logo" onClick={() => navigate("/")}>Rexy</h1>
-            {/* <img className="logo" src="../../logo/android-chrome-192x192.png" alt="logo" /> */}
-            <p className="tagline">A place to share your recommendations</p>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/search">
-                            {<img 
-                                className="icon" 
-                                src="../../images/magnifier.png" 
-                                alt="search"/>}
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/friends">
-                            {<img 
-                                className="icon"
-                                src="../../images/friends.png" 
-                                alt="friends"/>}
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/account">
-                            {<img 
-                                className="icon"
-                                src="../../images/user.png" 
-                                alt="user-account"/>}
-                        </Link>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    )
+  const navigate = useNavigate();
+  return (
+    <header>
+      <h1 className="logo" onClick={() => navigate("/")}>
+        Rexy
+      </h1>
+      {/* <img className="logo" src="../../logo/android-chrome-192x192.png" alt="logo" /> */}
+      <p className="tagline">A place to share your recommendations</p>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">
+              {
+                <img
+                  className="icon"
+                  src="../../images/home.png"
+                  alt="home"
+                />
+              }
+            </Link>
+          </li>
+          <li>
+            <Link to="/search">
+              {
+                <img
+                  className="icon"
+                  src="../../images/magnifier.png"
+                  alt="search"
+                />
+              }
+            </Link>
+          </li>
+          <li>
+            <Link to="/friends">
+              {
+                <img
+                  className="icon"
+                  src="../../images/friends.png"
+                  alt="friends"
+                />
+              }
+            </Link>
+          </li>
+          <li>
+            <Link to="/account">
+              {
+                <img
+                  className="icon"
+                  src="../../images/user.png"
+                  alt="user-account"
+                />
+              }
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
