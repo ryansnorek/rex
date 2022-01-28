8ed840c27e66e38062a6060871e2fbed48429a1c
Ryan Snorek <ryan.snorek@manamonitoring.com>
2022-01-28 09:16:39 -0500

    wip

--- src/hooks/useScrollSet.js
+++ src/hooks/useScrollSet.js
@@ -3,13 +3,14 @@ import { useLayoutEffect, useState } from "react";
 export default function useScrollSet() {
   const [scrollPosition, setScrollPosition] = useState(null);
 
-  useLayoutEffect(() => {
+  useLayoutEffect(
+    function lockScrollPosition() {
     const doc = document.documentElement;
     const top = (window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0);
     setScrollPosition(top);
     const originalStyle = window.getComputedStyle(document.body).overflow;
-    // document.body.style.overflow = "hidden";
-    // return () => (document.body.style.overflow = originalStyle);
+    document.body.style.overflow = "hidden";
+    return () => (document.body.style.overflow = originalStyle);
   }, []);
 
   return scrollPosition;
