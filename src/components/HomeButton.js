855af1b34d8aae1cda692eb0c0d6318a07bee4cb
Ryan Snorek <ryan.snorek@manamonitoring.com>
2022-02-06 11:31:17 -0500

    wip

--- src/components/HomeButton.js
+++ src/components/HomeButton.js
@@ -1,12 +1,14 @@
 export default function HomeButton({ displayType, title, handleToggleItem }) {
-    const typeSwitch = {
-        "Trending": "trending",
-        "Movies": "movies",
-        "TV Shows": "tvShows"
-    }
+  const typeSwitch = {
+    "Trending": "trending",
+    "Movies": "movies",
+    "TV Shows": "tvShows"
+  };
   return (
     <button
-      className={"navlink" + (displayType === typeSwitch[title] ? " activated" : "")}
+      className={
+        "navlink" + (displayType === typeSwitch[title] ? " activated" : "")
+      }
       onClick={handleToggleItem}
       name={typeSwitch[title]}
     >
