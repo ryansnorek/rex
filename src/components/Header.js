8c78aa55b5a29a2b9936185d0dc3fadeef2d2231
Ryan Snorek <ryan.snorek@manamonitoring.com>
2022-02-25 17:41:28 -0500

    wip

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ export default function Header() {
       <h1 className="logo" onClick={() => navigate("/")}>
         Rexy
       </h1>
-      <p className="tagline">A place to share your recommendations</p>
+      {/* <p className="tagline">A place to share your recommendations</p> */}
       <nav>
         <ul>
           <li>
