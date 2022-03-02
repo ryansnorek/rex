394add60d0cd4de2be05572119882ff1d94e2672
Ryan Snorek <ryan.snorek@manamonitoring.com>
2022-03-02 11:46:14 -0500

    avatars

--- src/components/WelcomeNewUser.js
+++ src/components/WelcomeNewUser.js
@@ -1,22 +1,32 @@
 import { connect } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { unsetFirstTimeUser } from "../actions";
+import { unsetFirstTimeUser, updateUser } from "../actions";
 import { AVATARS } from "../constants";
 
 function WelcomeNewUser({ user, dispatch }) {
   const navigate = useNavigate();
-  const handleClick = () => {
-    // dispatch(unsetFirstTimeUser());
-    // navigate("/");
+  const handleClick = (e) => {
+    const { user_id } = user;
+    dispatch(updateUser({ uploaded_image: e.target.name }, user_id))
+    dispatch(unsetFirstTimeUser());
+    navigate("/");
   };
   return (
     <div className="page">
       <div className="edit-profile">
-        <h2>Hey, {user.username}</h2>
+        <h2>Hey, {user.username}!</h2>
         <p>Select your avatar</p>
         <div className="avatars">
           {AVATARS.map((avatar) => {
-            return <img className="avatar" src={avatar} alt="avatar" />;
+            return (
+              <img
+                onClick={handleClick}
+                className="avatar"
+                name={avatar}
+                src={avatar}
+                alt="avatar"
+              />
+            );
           })}
         </div>
       </div>
