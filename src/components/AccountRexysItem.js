58f1e305dbb9e17426d02ac52519e2d617e1ba5b
Ryan Snorek <ryan.snorek@manamonitoring.com>
2022-03-07 10:33:38 -0500

    action buttons on rexys

--- src/components/AccountRexysItem.js
+++ src/components/AccountRexysItem.js
@@ -1,25 +1,33 @@
 import { POSTER_URL } from "../constants";
 
 function AccountRexysItem({ item }) {
-    return (
-        <div className="account-item">
-        <div className="poster">
-          {item.backdrop_path !== null ? (
-            <img src={`${POSTER_URL}${item.backdrop_path}`} alt="poster" />
-          ) : (
-            <img src="../../images/unavailable_poster.jpeg" alt="poster" />
-          )}
-        </div>
-        <div className="text">
-          <h3>{item.title || item.name}</h3>
-        </div>
-        {/* <UserContentActionBar
-          handleRating={handleRating}
-          handleSend={handleSend}
-          handleRemove={handleRemove}
-          item={item}
-        /> */}
+  return (
+    <div className="account-item">
+      <div className="poster">
+        {item.backdrop_path !== null ? (
+          <img src={`${POSTER_URL}${item.backdrop_path}`} alt="poster" />
+        ) : (
+          <img src="../../images/unavailable_poster.jpeg" alt="poster" />
+        )}
       </div>
-    );
-};
+      <div className="text">
+        <h3>{item.title || item.name}</h3>
+      </div>
+      <div className="actions">
+      <img
+          className="icon"
+        //   onClick={() => handleAddContent()}
+          src="../../images/add.png"
+          alt="add"
+        />
+        <img
+          className="icon delete"
+          src="../../images/close.png"
+          alt="delete"
+          // onClick={() => }
+        />
+      </div>
+    </div>
+  );
+}
 export default AccountRexysItem;
