3ae0aa2ad53491b61669437470787065d2c08537
Ryan Snorek <ryan.snorek@manamonitoring.com>
2022-04-02 17:58:33 -0400

    dry

--- src/components/AccountRexysItem.js
+++ src/components/AccountRexysItem.js
@@ -1,25 +1,21 @@
-import { POSTER_URL } from "../constants";
+import { POSTER_URL, UNAVAILABLE } from "../constants";
 
 function AccountRexysItem({ item }) {
   const poster = `${POSTER_URL}${item.backdrop_path}`;
   return (
     <div className="account-item">
       <div className="poster">
-        {item.backdrop_path !== null ? (
-          <img className="temp-placeholder" src={poster} alt="poster" />
-        ) : (
-          <img src="../../images/unavailable_poster.jpeg" alt="poster" />
-        )}
+        <img
+          className="temp-placeholder"
+          src={poster || UNAVAILABLE}
+          alt="poster"
+        />
       </div>
       <div className="text">
         <h3>{item.title || item.name}</h3>
       </div>
       <div className="actions">
-      <img
-          className="icon"
-          src="../../images/add.png"
-          alt="add"
-        />
+        <img className="icon" src="../../images/add.png" alt="add" />
         <img
           className="icon delete"
           src="../../images/close.png"
